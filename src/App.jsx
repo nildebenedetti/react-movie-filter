335aaf6c62da93935eeeaf0c0bdc77a6acd55822
“Nilde <nilde.benedetti@gmail.com>
2026-05-06 16:47:07 +0200

    fix: process notes + transform map in ul in funzione in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,26 @@ function App() {
   {/* variabile di stato per settare il valore della select, dove il default
   sara'il valore che vediamo preselezionato */}
   const [selectOptionValue, setSelectOptionValue] = useState("Scegli il genere");
-
+  // adesso devo mettere la funzione useEffect, 
+  // e far si che quando cambia la select option value CAMBI LA FUNZIONE NELLA MAP
+  // AL CAMBIAMENTO DI STATO deve attivarsi quanto segue
+  
+  // sposto la map in una funzione
+  const renderPlainList = (movieArray) => 
+      movieArray.map((movie) => {
+        return <li key={movie.id}>
+          {movie.title}
+        </li>
+});
+    
+    
+  // aggiungere una validazione (magari con terna logica)
+    // SE value === selectOptionValue 
+        // ALLORA mostriamo funzione con map originaria
+    // SE value !== 
+      // ALLORA la funzione con filtro movie.genre
+        // filter per genere su lista oggetti in const. filteredList
+            // POI map
   
   return <div className="card m-5 px-5 py-3">
     <h5 className="card-title my-4">Cosa guardiamo stasera?</h5>
@@ -26,11 +45,7 @@ function App() {
       <option value="Azione">Azione</option>
     </select>
     <ul className="py-3">
-      {movies.map((movie) => {
-        return <li key={movie.id}>
-          {movie.title}
-        </li>
-      })}
+      {renderPlainList(movies)}
     </ul>
   </div>
 }
