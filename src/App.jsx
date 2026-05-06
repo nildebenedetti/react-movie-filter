ffca30c1512aa21ab3ff32cc0a83a0f8ae6923df
“Nilde <nilde.benedetti@gmail.com>
2026-05-06 15:39:49 +0200

    add: card con select per scegliere filtro genere

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import movies from "./data/movies";
 
 function App() {
-  return ;
+  return <div className="card mx-auto py-3">
+    <select className="form-select" aria-label="Default select">
+      <option selected>Scegli il genere</option>
+      <option value="Fantascienza">Fantascienza</option>
+      <option value="Thriller">Thriller</option>
+      <option value="Romantico">Romantico</option>
+      <option value="Azione">Azione</option>
+    </select>
+  </div>;
 }
 export default App;
