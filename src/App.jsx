e4faa325bfaff747bbbc3676601f57a0bee4a122
“Nilde <nilde.benedetti@gmail.com>
2026-05-06 18:11:36 +0200

    add: variabile dis stato per lista film

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   sara'il valore che vediamo preselezionato */}
   const [selectOptionValue, setSelectOptionValue] = useState("Scegli il genere");
   // variabile per lista filtrata
- const [ moviesFiltered, setMoviesFiltered ] = useState([]);
+ const [ moviesFiltered, setMoviesFiltered ] = useState(movies);
   // sposto la map in una funzione
   const renderPlainList = (movieArray) =>
     movieArray.map((movie) => {
@@ -19,19 +19,17 @@ function App() {
     });
 
   // La mia useEffect collegata al valore di option value dovra'fare questo:
-  // aggiungere una validazione (magari con terna logica)
   // SE value di selectOptionValue === scegli...
-  // ALLORA mostriamo funzione con map originaria
+  // ALLORA moviesFiltered = movies
   // SE value !== 
-  // ALLORA la funzione con filtro movie.genre
-  // filter per genere su lista oggetti in const. filteredList
-  // POI map
+  // ALLORA la funzione con filter movie.genre
+  // filter per genere su lista oggetti in const. filteredMovies
+  // POI passo la list a map
   useEffect(
     () => { // primo parametro una callback che non prende parameretri e restituisce una clean-up callback
 
       console.log('sono la funzione!');
-     
-
+    
       if (selectOptionValue !== 'Scegli il genere') {
         // filtra la lista da qui e crea copia della lista originaria
         const filteredByGenre = movies.filter((movie) =>
@@ -65,9 +63,6 @@ function App() {
     <ul className="py-3">
       {renderPlainList(moviesFiltered)} {/** Ti passero' filteredMovies */}
     </ul>
-    <p>
-      {`Il valore di ${selectOptionValue} e ${moviesFiltered}`}
-    </p>
   </div>
 }
 export default App;
